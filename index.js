ca68cf4a02a0b3566488651426b5e2b3cd0e4e2c
IgnatiusFernando-Denamika <f_ignatius@ymail.com>
2019-01-28 15:27:48 +0700

    first

--- index.js
+++ index.js
@@ -54,24 +54,24 @@ app.get('/getListMovieCategorie',(req,res)=>{
 //////////////////////////////////////////END METHOD GET /////////////////////////////////////////
 /////////////////////////////////////////START METHOD POST//////////////////////////////////////
 app.post('/addMovie', (req,res) => {
-    var sql = ` INSERT into movies SET ? `;  
+    var {nama,tahun,description} = req.body;
+    var sql = ` INSERT into movies values(null,'${nama}','${tahun}','${description}') `;  
     conn.query(sql,(err, result) => {////////////ADD TABLE MOVIES//////////////////////////
         if(err) throw err ;
         res.send(result);
     })
 })
 app.post('/addCategories', (req,res) => {
-    var sql = `insert into categories set ?`;  
+    var {nama} = req.body
+    var sql = `insert into categories values(null,'${nama}')`;  
     conn.query(sql,(err, result) => {///////////////ADD TABLE CATEGORIES//////////////////
         res.send(result);
     })
 })
 
 app.post('/addMovCat', (req,res) => {
-    var idMovie = req.body.idMovie;
-    var idcategory = req.body.idcategory;
-    var sql = `insert into movcat select m.id as idmovie ,c.id as idcategory from movies m join categories c on c.id
-    where (m.id ='${idMovie}' and c.id ='${idcategory}');`;  
+    var {idmovie, idcategory} = req.body;
+    var sql = `insert into movcat values ('${idmovie},'${idcategory}');`;  
     
     conn.query(sql,(err, result) => {///////////ADD TABLE LIST MOvCAT//////////////////
         if(err) throw err;
@@ -82,42 +82,67 @@ app.post('/addMovCat', (req,res) => {
 /////////////////////////////////START Method PUT(EDIT)////////////////////////////////////
 app.put('/editMovies/:id',(req,res)=>{
     var MovieId = req.params.id;
+    var {nama = "", tahun ="", description=""} = req.body
+
     var sql =`SELECT * from movies where id = ${MovieId};`;
+
     conn.query(sql, (err, result) => {//////////EDIT TABLE MOVIE/////////////////////////
         if(err) throw err;
-        sql = `Update movies set nama='${data.nama}' where id = ${MovieId};`
+        if(result.length>0){
+            if(nama == ""){nama = result[0].nama}
+            if ( tahun == ""){tahun = result[0].tahun}
+            if(description =="") {description == result[0].description};
+        }
+        sql = `Update movies set nama='${nama}',tahun ='${tahun}',
+        description ='${description}' where id = ${MovieId};`
     })
 })
 
 app.put('/editCategories/:id',(req,res)=>{
     var CategoryId = req.params.id;
+    var {nama} = req.body
+
     var sql =`SELECT * from categories where id = ${CategoryId};`;
+
     conn.query(sql, (err, result) => {//////////EDIT TABLE Categories///////////////////
         if(err) throw err;
-        sql = `Update categories set nama='${data.nama}' where id = ${CategoryId};`
+        sql = `Update categories set nama='${nama}' where id = ${CategoryId};`
         res.send(result);
     })
 })
 /////////////////////////////////END METHOD PUT(EDIT)//////////////////////////////////////
 /////////////////////////////////START METHOD DELETE///////////////////////////////////////
 app.delete('/deleteMovie/:id',(req,res)=>{
-    var sql =`SELECT * from movies where id = ${MovieId};`;
+    var idMovie = req.params.id
+    var sql =`Delete from movies where id ='${idMovie}';`;
     conn.query(sql, (err, result) => {//////////DELETE TABLE MOVIE///////////////////
         if(err) throw err;
+        sql = `delete from movcat where idmovie='${idMovie}';`;
+        conn.query(sql,(err1,result1)=>{
+            if(err1) throw err1;
+            res.send(result1);
+        })
         res.send(result);
     })
 })
 
 app.delete('/deleteCategorie/:id',(req,res)=>{
-    var sql =`SELECT * from categories where id = ${CategoryId};`;
+    var idCategory = req.params/id
+    var sql =`delete from categories where id = '${idCategory}';`;
         conn.query(sql, (err, result) => {//////////DELETE TABLE CATEGORIE///////////////////
         if(err) throw err;
+        sql = `delete from movcat where idcategory ='${idCategory}';`;
+        conn.query(sql,(err1,result1)=>{
+            if(err1) throw err1;
+            res.send(result1);
+        })
         res.send(result);
     })
 })
 
 app.delete('/deleteMovCat',(req,res)=>{
-    var sql =``;
+    var {idcategory, idmovie} = req.body
+    var sql =`delete from movcat where idmovie = ${idmovie}and idcategory ='${idcategory}'`;
     conn.query(sql, (err, result) => {//////////DELETE TABLE MOVIE CATEGORIE///////////////////
         if(err) throw err;
         res.send(result);
